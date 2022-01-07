9631e2df7552d2551d1093083b111f1c5576d349
Den <zgibex333@gmail.com>
2022-01-07 22:35:05 +0300

    first commit ep 13

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Coca-Cola_logo.svg/1200px-Coca-Cola_logo.svg.png"
+                alt="logo"/>
+        </header>
+    )
+}
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+                    alt="content"/>
+            </div>
+            <div className="">
+                avatar/description
+            </div>
+            <div className="">
+                posts
+            </div>
+        </div>
+    )
+}
